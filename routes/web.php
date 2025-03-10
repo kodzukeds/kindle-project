11337205fefe69ceb16ca78db400b1c5b46ade4f
kodzukeds <edumunoz2002@gmail.com>
2025-03-10 23:29:15 +0100

    feat: added routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookmarkController;
+use App\Http\Controllers\DownloadController;
+use App\Http\Controllers\HighlightedQuoteController;
+use App\Http\Controllers\KindleController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\ReadingProgressController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +23,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('authors', AuthorController::class);
+    Route::resource('books', BookController::class);
+    Route::resource('bookmarks', BookmarkController::class);
+    Route::resource('downloads', DownloadController::class);
+    Route::resource('highlightedQuotes', HighlightedQuoteController::class);
+    Route::resource('kindles', KindleController::class);
+    Route::resource('publishers', PublisherController::class);
+    Route::resource('reading_progress', ReadingProgressController::class);
+    Route::post('/books/{book}/kindles/{kindle}/attach', [BookController::class, 'attachKindle'])->name('books.attachKindle');
+    Route::post('/books/{book}/kindles/{kindle}/detach', [BookController::class, 'detachKindle'])->name('books.detachKindle');
+    Route::post('/books/{book}/authors/{author}/attach', [BookController::class, 'attachAuthor'])->name('books.attachAuthor');
+    Route::post('/books/{book}/authors/{author}/detach', [BookController::class, 'detachAuthor'])->name('books.detachAuthor');
+    Route::post('/kindles/{kindle}/books/{book}/attach', [KindleController::class, 'attachBook'])->name('kindles.attachBook');
+    Route::post('/kindles/{kindle}/books/{book}/detach', [KindleController::class, 'detachBook'])->name('kindles.detachBook');
+
+
 });
 
 require __DIR__.'/auth.php';
